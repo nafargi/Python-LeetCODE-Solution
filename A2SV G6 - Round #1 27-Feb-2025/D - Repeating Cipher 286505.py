b5f900d131d17129e94ef0517ad0f4837c7d1b23
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2025-02-27 23:29:54 +0300

    Created 'D - Repeating Cipher' by A2SV Hub

---
+++ A2SV G6 - Round #1 27-Feb-2025/D - Repeating Cipher 286505.py
@@ -0,0 +1,16 @@
+# Problem: D - Repeating Cipher - https://codeforces.com/gym/585107/problem/D
+
+n =int(input())
+s =input()
+
+ans = []
+currentIndex = 0
+counter = 0
+
+while counter < n:
+    if currentIndex < n:
+       ans.append(s[currentIndex])
+    counter += 1
+    currentIndex += counter
+   
+print("".join(ans))
