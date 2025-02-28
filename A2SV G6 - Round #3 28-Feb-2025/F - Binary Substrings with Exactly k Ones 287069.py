70808f0abd01076afd91bf5dd68cd5e35339b626
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2025-02-28 19:20:05 +0300

    Created 'F - Binary Substrings with Exactly k Ones' by A2SV Hub

---
+++ A2SV G6 - Round #3 28-Feb-2025/F - Binary Substrings with Exactly k Ones 287069.py
@@ -0,0 +1,18 @@
+# Problem: F - Binary Substrings with Exactly k Ones - https://codeforces.com/gym/588468/problem/F
+
+from collections import Counter
+k = int(input())
+s= input().strip()
+
+sum = 0
+counter = 0
+d = {0:0}
+
+for i in range(len(s)):
+   if s[i] == '1':
+       sum += 1
+   if sum == k:
+       counter = counter + 1
+   counter += d.get(sum - k,0)
+   d[sum] = d.get(sum,0) + 1
+print(counter)
