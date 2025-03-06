3e138e38c53abea4bb48ae5cbb6436439e978730
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2025-03-06 18:15:26 +0300

    Created 'Daily Temperatures' by A2SV Hub

---
+++ 06-Mar-2025/Daily Temperatures 294736.py
@@ -0,0 +1,12 @@
+# Problem: Daily Temperatures - https://leetcode.com/problems/daily-temperatures/
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        ans = [0] * len(temperatures)
+        st = []
+        for i in range(len(temperatures)):
+          while st and st[-1][0] < temperatures[i]:
+             idx = st.pop()
+             ans[idx[1]] =  i - idx[1]
+          st.append((temperatures[i],i))
+        return ans
