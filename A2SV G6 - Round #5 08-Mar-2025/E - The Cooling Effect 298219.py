e065a0afacfae79a74e701605d062f3637fbd0b3
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2025-03-08 23:00:02 +0300

    Created 'E - The Cooling Effect' by A2SV Hub

---
+++ A2SV G6 - Round #5 08-Mar-2025/E - The Cooling Effect 298219.py
@@ -0,0 +1,23 @@
+# Problem: E - The Cooling Effect - https://codeforces.com/gym/591928/problem/E
+
+
+t = int(input())  
+for _ in range(t):
+    input()
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    temp = list(map(int, input().split()))
+
+#i was thouhgt it is simple to do it but i'm cooked bro
+    ans = [float('inf')] * n 
+
+    for j in range(k):
+        ans[a[j] - 1] = temp[j]  
+
+    for i in range(1, n):
+        ans[i] = min(ans[i], ans[i - 1] + 1)
+
+    for i in range(n - 2, -1, -1):
+        ans[i] = min(ans[i], ans[i + 1] + 1)
+
+    print(*ans)
