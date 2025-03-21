8b13611bebc886e2b06324319e5fa4eb7e323c67
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2025-03-21 19:07:08 +0300

    Created 'Permutations' by A2SV Hub

---
+++ 21-Mar-2025/Permutations 314852.py
@@ -0,0 +1,22 @@
+# Problem: Permutations - https://leetcode.com/problems/permutations/
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+            ans = []
+            temp = []
+            
+            def backtrack(temp,nums):
+                if len(temp) == len(nums):
+                    ans.append(temp[:])
+                    return
+
+                for i in range(len(nums)):
+                    if nums[i] in temp:
+                        continue
+                    temp.append(nums[i])
+                    backtrack(temp,nums)
+                    temp.pop()
+                #printzzzzzzzzzzzzzzzzzzzzzz
+            backtrack(temp,nums)
+            return ans
+            
